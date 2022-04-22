e71c5d3afc0b097e7b6590ccf659f9b1cee626e1
hjcestaro <hjcestaro@hotmail.com>
2022-04-22 12:04:16 -0300

    feature(CU-23tgan9): implementado a tag de slot no componente header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,27 +2,18 @@
   <header id="header" data-state="inactive">
     <div class="container">
       <div class="box-logo">
-       <span>Coloque Sua imagem Aqui</span>
+        <span>Coloque Sua imagem Aqui</span>
       </div>
 
-      <nav class="menu-header">
-        <ul>
-          <li><a class="menu-option" href="#">Features</a></li>
-          <li><a class="menu-option" href="#">Sobre</a></li>
-          <li><a class="menu-option" href="#">Preços</a></li>
-          <li><a class="menu-option" href="#">Contato</a></li>
-          <li class="login-button"><a href="./login.html">Entrar</a></li>
-        </ul
-        >
-      </nav>
+     <slot></slot>
     </div>
   </header>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'AppHeader',
-})
+  name: "AppHeader",
+});
 </script>
